493a50368091bacf67fd0cbc6965dc4471340163
dayalan <daya.22.thilak@gmail.com>
2024-11-04 17:27:54 +0530

    intial commit

---
+++ src/main/java/com/product/app/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.product.app.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    public void getProductDetails(){}
+}
